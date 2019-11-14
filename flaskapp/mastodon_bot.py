e9ceb1b9be158cdd4962b2270d9c5bbe20071770
larkeith <llarkeith@gmail.com>
2019-11-14 15:06:04 -0800

    Get all statuses, use non-authed client for statuses
    Now continues paginating until `count` statuses have been retrieved
    Also uses a non-authed client for statuses, due to rate limiting

--- flaskapp/mastodon_bot.py
+++ flaskapp/mastodon_bot.py
@@ -7,6 +7,10 @@ import os
 from decouple import config
 from mastodon import Mastodon
 from flaskapp.model import Account, Status
+from flaskapp.exceptions import (
+	NoSearchResultsError,
+	NotInDatabaseError
+)
 
 
 BOT_LOG = logging.getLogger('root')
@@ -38,6 +42,7 @@ def instantiate_bot() -> Mastodon:
 		client_id=config('MASTODON_SECRET_LOCATION'),
 		access_token=config('MASTODON_TOKEN_LOCATION'),
 		api_base_url=config('MASTODON_API'),
+		ratelimit_method='throw',
 	)
 
 	return (client)
@@ -54,21 +59,60 @@ def get_account(client, username):
 	# raise exception if ambiguous
 	BOT_LOG.info(f'Getting account for username {username}...')
 	results = client.search(username, result_type='accounts')
-	return(results['accounts'][0])
+	if len(results['accounts']) < 1:
+		raise NoSearchResultsError(f'No results for account search with username {username}')
+	return (results['accounts'][0])
 
 
-def get_statuses(client, *, account=None, username=None, count=20):
+def get_statuses(*, client=None, account=None, username=None, count=None, limit=40):
+	"""
+	Get the statuses for an account, by account dict or username
+
+	Args:
+		client (mastodon.Mastodon, optional): Mastodon.py client to use.
+			If not provided, will initialize a new, non-authenticated client.
+			Strongly recommended *against* passing, as non-authed clients have higher rate limits.
+		account (dict, optional): Account dict. Precisely one of this or `username` is required.
+		username (str, optional): Username. Precisely one of this or `account` is required.
+		count (int, optional): Number of statuses to fetch. If omitted, retrieves all.
+		limit (int, optional): Number of statuses to fetch per request. Defaults to 40.
+			This is usually limited to 40 serverside.
+
+	Returns:
+		list: Statuses retrieved
+	"""
+
 	assert (account is None) != (username is None), \
 		f'''
 		get_statuses requires precisely one of `account`, `username`
 		received `account`={account}, `username`={username}
 		'''
 
+	if client is None:
+		client = Mastodon(
+			api_base_url=config('MASTODON_API'),
+			ratelimit_method='throw',
+		)
+
 	if account is None:
 		account = get_account(client, username)
 	BOT_LOG.info(f'Getting statuses for account id {account["id"]}...')
-	statuses = client.account_statuses(id=account['id'])
-	return(statuses[:count])
+	full_statuses = []
+	max_id = None
+	while True:
+		BOT_LOG.info(f'Getting statuses for account id {account["id"]} with max_id={max_id}. Current count: {len(full_statuses)}')
+		statuses = client.account_statuses(id=account['id'], max_id=max_id, limit=limit)
+		if len(statuses) > 0:
+			full_statuses += statuses
+			max_id = statuses[-1]['id']
+		else:
+			break
+		if count is not None and count <= len(full_statuses):
+			break
+	BOT_LOG.info(f'Got {len(full_statuses)} statuses.')
+	if count is None:
+		return (full_statuses)
+	return (full_statuses[:count])
 
 
 def instantiate_basilica():
@@ -87,13 +131,16 @@ def get_db_account(db, mastodon_client, username):
 	BOT_LOG.info(f'Getting database info for username {username}...')
 	account = get_account(mastodon_client, username)
 	db_account = Account.query.get(str(account['id']))
+	if db_account is None:
+		raise NotInDatabaseError(
+			f'No information in database for account with username {username}'
+		)
 	return (db_account)
 
 
 def get_db_statuses(db, mastodon_client, username):
 	BOT_LOG.info(f'Getting database statuses for username {username}...')
-	account = get_account(mastodon_client, username)
-	db_account = Account.query.get(str(account['id']))
+	db_account = get_db_account(db, mastodon_client, username)
 	return ([status.content for status in db_account.statuses])
 
 
@@ -104,7 +151,7 @@ def get_db_embeddings(db, mastodon_client, username):
 	return ([status.embedding for status in db_account.statuses])
 
 
-def add_account(db, basilica_client, mastodon_client, username, count=20):
+def add_account(db, basilica_client, mastodon_client, username, count=None):
 	BOT_LOG.info(f'Adding account {username}...')
 	account = get_account(mastodon_client, username)
 
@@ -118,7 +165,7 @@ def add_account(db, basilica_client, mastodon_client, username, count=20):
 		)
 	db.session.add(db_account)
 
-	statuses = get_statuses(mastodon_client, account=account, count=count)
+	statuses = get_statuses(account=account, count=count)
 
 	contents = [status['content'] for status in statuses]
 	embeddings = list(get_embeddings(basilica_client, contents))
@@ -138,4 +185,6 @@ def add_account(db, basilica_client, mastodon_client, username, count=20):
 
 	db.session.commit()
 	BOT_LOG.info(f'Done adding account.')
+
+	return (account)
 
